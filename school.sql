4368bae7a43e05186ae98d53f98d87f4d97305ec
qaxwe <47267253+qaxwe@users.noreply.github.com>
2019-02-02 10:19:30 +0300

    added sql backup

---
+++ school.sql
@@ -0,0 +1,201 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Feb 02, 2019 at 07:18 AM
+-- Server version: 5.7.23
+-- PHP Version: 7.2.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `school`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin_login`
+--
+
+DROP TABLE IF EXISTS `admin_login`;
+CREATE TABLE IF NOT EXISTS `admin_login` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `class`
+--
+
+DROP TABLE IF EXISTS `class`;
+CREATE TABLE IF NOT EXISTS `class` (
+  `class_id` int(11) NOT NULL AUTO_INCREMENT,
+  `class_name` varchar(50) NOT NULL,
+  `teacher_id` varchar(50) NOT NULL,
+  `teacher_name` varchar(50) NOT NULL,
+  `std_id` varchar(50) NOT NULL,
+  `std_name` varchar(50) NOT NULL,
+  `semester` varchar(50) NOT NULL,
+  `description` varchar(50) NOT NULL,
+  PRIMARY KEY (`class_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `exams`
+--
+
+DROP TABLE IF EXISTS `exams`;
+CREATE TABLE IF NOT EXISTS `exams` (
+  `exam_id` int(11) NOT NULL AUTO_INCREMENT,
+  `exam_name` varchar(50) NOT NULL,
+  `exam_type` varchar(50) NOT NULL,
+  `class_id` varchar(50) NOT NULL,
+  `date` varchar(50) NOT NULL,
+  `year` varchar(50) NOT NULL,
+  PRIMARY KEY (`exam_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `exam_marks`
+--
+
+DROP TABLE IF EXISTS `exam_marks`;
+CREATE TABLE IF NOT EXISTS `exam_marks` (
+  `em_id` int(11) NOT NULL AUTO_INCREMENT,
+  `em_name` varchar(50) NOT NULL,
+  `em_type` varchar(50) NOT NULL,
+  `em_class` varchar(50) NOT NULL,
+  `em_std` varchar(50) NOT NULL,
+  `em_date` varchar(50) NOT NULL,
+  PRIMARY KEY (`em_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `parent`
+--
+
+DROP TABLE IF EXISTS `parent`;
+CREATE TABLE IF NOT EXISTS `parent` (
+  `parent_id` int(11) NOT NULL AUTO_INCREMENT,
+  `p_name` varchar(50) NOT NULL,
+  `p_type` varchar(50) NOT NULL,
+  `p_email` varchar(50) NOT NULL,
+  `p_phone` varchar(50) NOT NULL,
+  PRIMARY KEY (`parent_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+DROP TABLE IF EXISTS `student`;
+CREATE TABLE IF NOT EXISTS `student` (
+  `std_id` int(11) NOT NULL AUTO_INCREMENT,
+  `std_name` varchar(50) NOT NULL,
+  `std_rollno` varchar(50) NOT NULL,
+  `std_phone` varchar(50) NOT NULL,
+  `std_age` varchar(50) NOT NULL,
+  `std_email` varchar(50) NOT NULL,
+  `pob` varchar(50) NOT NULL,
+  `dob` varchar(50) NOT NULL,
+  `gender` varchar(50) NOT NULL,
+  `semester` varchar(50) NOT NULL,
+  `year` varchar(50) NOT NULL,
+  `p_name` varchar(50) NOT NULL,
+  `p_id` varchar(50) NOT NULL,
+  `sub_date` varchar(50) NOT NULL,
+  PRIMARY KEY (`std_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_login`
+--
+
+DROP TABLE IF EXISTS `student_login`;
+CREATE TABLE IF NOT EXISTS `student_login` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subjects`
+--
+
+DROP TABLE IF EXISTS `subjects`;
+CREATE TABLE IF NOT EXISTS `subjects` (
+  `sub_id` int(11) NOT NULL AUTO_INCREMENT,
+  `sub_name` varchar(50) NOT NULL,
+  `sub_description` varchar(50) NOT NULL,
+  PRIMARY KEY (`sub_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teacher`
+--
+
+DROP TABLE IF EXISTS `teacher`;
+CREATE TABLE IF NOT EXISTS `teacher` (
+  `t_id` int(11) NOT NULL AUTO_INCREMENT,
+  `t_name` varchar(50) NOT NULL,
+  `qualification` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `phone` varchar(50) NOT NULL,
+  `joined_year` varchar(50) NOT NULL,
+  `class_name` varchar(50) NOT NULL,
+  `hire_date` varchar(50) NOT NULL,
+  `description` varchar(100) NOT NULL,
+  PRIMARY KEY (`t_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teacher_login`
+--
+
+DROP TABLE IF EXISTS `teacher_login`;
+CREATE TABLE IF NOT EXISTS `teacher_login` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
