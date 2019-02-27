a28df546dcefb84356485c979db913375b48b865
Mohamed Abdiqadir Osman <mohaaosman12@gmail.com>
2019-02-27 09:18:31 +0300

    admin link fixed

--- Admin/Layout/admin_layout.php
+++ Admin/Layout/admin_layout.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 
-  <head>
+<head>
 
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -17,143 +17,140 @@
     <!-- Custom fonts for this template-->
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
 
-   
+
     <!-- Custom styles for this template-->
     <link href="css/sb-admin.css" rel="stylesheet">
 
-  </head>
+</head>
 
-  <body id="page-top">
+<body id="page-top">
 
     <nav class="navbar navbar-expand navbar-dark bg-dark static-top">
 
-      <a class="navbar-brand mr-1" href="index.php">Dr Ahmed College</a>
+        <a class="navbar-brand mr-1" href="http://localhost/xampserver/drcol/admin">Dr Ahmed College</a>
 
-      <button class="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="#">
-        <i class="fas fa-bars"></i>
-      </button>
+        <button class="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="#">
+            <i class="fas fa-bars"></i>
+        </button>
 
-      <!-- Navbar Search -->
-      <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
-        <div class="input-group">
-          <input type="text" class="form-control" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
-          <div class="input-group-append">
-            <button class="btn btn-primary" type="button">
-              <i class="fas fa-search"></i>
-            </button>
-          </div>
-        </div>
-      </form>
+        <!-- Navbar Search -->
+        <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
+            <div class="input-group">
+                <input type="text" class="form-control" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
+                <div class="input-group-append">
+                    <button class="btn btn-primary" type="button">
+                        <i class="fas fa-search"></i>
+                    </button>
+                </div>
+            </div>
+        </form>
 
-      <!-- Navbar -->
-      <ul class="navbar-nav ml-auto ml-md-0">
+        <!-- Navbar -->
+        <ul class="navbar-nav ml-auto ml-md-0">
 
-        <li class="nav-item dropdown no-arrow">
-          <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <i class="fas fa-user-circle fa-fw"></i>
-          </a>
-          <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
-            <a class="dropdown-item" href="#">Settings</a>
-            <a class="dropdown-item" href="#">Activity Log</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">Logout</a>
-          </div>
-        </li>
-      </ul>
+            <li class="nav-item dropdown no-arrow">
+                <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <i class="fas fa-user-circle fa-fw"></i>
+                </a>
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
+                    <a class="dropdown-item" href="#">Settings</a>
+                    <a class="dropdown-item" href="#">Activity Log</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">Logout</a>
+                </div>
+            </li>
+        </ul>
 
     </nav>
 
     <div id="wrapper">
 
-      <!-- Sidebar -->
-      <ul class="sidebar navbar-nav">
-        <li class="nav-item active">
-          <a class="nav-link" href="index.php">
-            <i class="fas fa-fw fa-tachometer-alt"></i>
-            <span>Dashboard</span>
-          </a>
-        </li>
-       
-        <li class="nav-item">
-          <a class="nav-link" href="http://localhost/xampserver/drcol/admin/Assignments.php">
-            <i class="fa fa-file-pdf"></i>
-            <span>Assignments</span></a>
-        </li>
+        <!-- Sidebar -->
+        <ul class="sidebar navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="index.php">
+                    <i class="fas fa-fw fa-tachometer-alt"></i>
+                    <span>Dashboard</span>
+                </a>
+            </li>
 
-          <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <i class="fas fa-users"></i>
-            <span>Students</span>
-          </a>
-          <div class="dropdown-menu" aria-labelledby="pagesDropdown">
-            <a class="dropdown-item" href="http://localhost/xampserver/drcol/admin/students/new.php">New</a>
-            <a class="dropdown-item" href="http://localhost/xampserver/drcol/admin/students/all.php">All</a>
-           
-        </li>
-          
-               <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <i class="fa fa-sitemap"></i>
-            <span>Classes</span>
-          </a>
-          <div class="dropdown-menu" aria-labelledby="pagesDropdown">
-            <a class="dropdown-item" href="http://localhost/xampserver/drcol/admin/classes/new.php">New</a>
-            <a class="dropdown-item" href="http://localhost/xampserver/drcol/admin/classes/all.php">All</a>
-              
-        </li>
-          
+            <li class="nav-item">
+                <a class="nav-link" href="http://localhost/xampserver/drcol/admin/Assignments.php">
+                    <i class="fa fa-file-pdf"></i>
+                    <span>Assignments</span></a>
+            </li>
 
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <i class="fas fa-user-tie"></i>
-            <span>Teachers</span>
-          </a>
-          <div class="dropdown-menu" aria-labelledby="pagesDropdown">
-            <a class="dropdown-item" href="http://localhost/xampserver/drcol/admin/teachers/new.php">New</a>
-            <a class="dropdown-item" href="http://localhost/xampserver/drcol/admin/teachers/all.php">All</a>
-        </li>    
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <i class="fas fa-users"></i>
+                    <span>Students</span>
+                </a>
+                <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+                    <a class="dropdown-item" href="http://localhost/xampserver/drcol/admin/students/new.php">New</a>
+                    <a class="dropdown-item" href="http://localhost/xampserver/drcol/admin/students/all.php">All</a>
 
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <i class="fa fa-book"></i>
-            <span>Subjects</span>
-          </a>
-          <div class="dropdown-menu" aria-labelledby="pagesDropdown">
-            <a class="dropdown-item" href="http://localhost/xampserver/drcol/admin/subjects/new.php">New</a>
-            <a class="dropdown-item" href="http://localhost/xampserver/drcol/admin/subjects/all.php">All</a>
-        </li>
-       
+            </li>
 
-        <li class="nav-item">
-          <a class="nav-link" href="http://localhost/xampserver/drcol/admin/events.php">
-            <i class="fa fa-bullhorn"></i>
-            <span>Annoucements</span></a>
-        </li>
-    
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <i class="fa fa-graduation-cap"></i>
-            <span>Exams</span>
-          </a>
-          
-          <div class="dropdown-menu" aria-labelledby="pagesDropdown">
-            <a class="dropdown-item" href="http://localhost/xampserver/drcol/admin/exams/new.php">New</a>
-            <a class="dropdown-item" href="http://localhost/xampserver/drcol/admin/exams/all.php">All</a>
-        </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <i class="fa fa-sitemap"></i>
+                    <span>Classes</span>
+                </a>
+                <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+                    <a class="dropdown-item" href="http://localhost/xampserver/drcol/admin/classes/new.php">New</a>
+                    <a class="dropdown-item" href="http://localhost/xampserver/drcol/admin/classes/all.php">All</a>
 
-        <li class="nav-item">
-          <a class="nav-link" href="http://localhost/xampserver/drcol/admin/schedule.php">
-            <i class="fas fa-fw fa-table"></i>
-            <span>Classes Schedule</span></a>
-        </li>
-
-        <li class="nav-item">
-          <a class="nav-link" href="#">
-            <i class="fas fa-fw fa-table"></i>
-            <span>Management </span></a>
-        </li>
-
-      </ul>
+            </li>
 
 
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <i class="fas fa-user-tie"></i>
+                    <span>Teachers</span>
+                </a>
+                <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+                    <a class="dropdown-item" href="http://localhost/xampserver/drcol/admin/teachers/new.php">New</a>
+                    <a class="dropdown-item" href="http://localhost/xampserver/drcol/admin/teachers/all.php">All</a>
+            </li>
 
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <i class="fa fa-book"></i>
+                    <span>Subjects</span>
+                </a>
+                <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+                    <a class="dropdown-item" href="http://localhost/xampserver/drcol/admin/subjects/new.php">New</a>
+                    <a class="dropdown-item" href="http://localhost/xampserver/drcol/admin/subjects/all.php">All</a>
+            </li>
+
+
+            <li class="nav-item">
+                <a class="nav-link" href="http://localhost/xampserver/drcol/admin/events.php">
+                    <i class="fa fa-bullhorn"></i>
+                    <span>Annoucements</span></a>
+            </li>
+
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <i class="fa fa-graduation-cap"></i>
+                    <span>Exams</span>
+                </a>
+
+                <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+                    <a class="dropdown-item" href="http://localhost/xampserver/drcol/admin/exams/new.php">New</a>
+                    <a class="dropdown-item" href="http://localhost/xampserver/drcol/admin/exams/all.php">All</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="http://localhost/xampserver/drcol/admin/schedule.php">
+                    <i class="fas fa-fw fa-table"></i>
+                    <span>Classes Schedule</span></a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <i class="fas fa-fw fa-table"></i>
+                    <span>Management </span></a>
+            </li>
+
+        </ul>
